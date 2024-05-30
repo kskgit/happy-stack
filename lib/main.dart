41f2ebb1a32f5012cb608c46c533297a98b2a604
nakao keisuke <r3grashopper193@gmail.com>
2024-05-30 21:38:19 +0900

    雛形ページ作成

--- lib/main.dart
+++ lib/main.dart
@@ -13,134 +13,122 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  FlutterAppAuth appAuth = const FlutterAppAuth();
-
-  void _incrementCounter() {
-    auth() async {
-      try {
-        final AuthorizationTokenResponse? result = await appAuth.authorizeAndExchangeCode(
-          AuthorizationTokenRequest(
-            dotenv.get('CLIENT_ID'),
-            dotenv.get('REDIRECT_URL'),
-            discoveryUrl: dotenv.get('DISCOVERY_URL'),
-            scopes: ['openid'],
-          ),
-        );
-        print(result?.accessToken);
-      } catch (e) {
-        print('Error during authorization: $e');
-        throw e;
-      }
-    }
-    auth();
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('トップページ'),
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(text: '月'),
+              Tab(text: '火'),
+              Tab(text: '水'),
+              Tab(text: '木'),
+              Tab(text: '金'),
+              Tab(text: '土'),
+              Tab(text: '日'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            DayView(),
+            DayView(),
+            DayView(),
+            DayView(),
+            DayView(),
+            DayView(),
+            DayView(),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DayView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: [
+        EventCard(
+          icon: Icons.emoji_emotions,
+          text: '呪術廻戦',
+          iconBgColor: Colors.orange,
+        ),
+        EventCard(
+          icon: Icons.coffee,
+          text: '朝からハンドドリップでコーヒー淹れる',
+          iconBgColor: Colors.brown,
+        ),
+        EventCard(
+          icon: Icons.favorite,
+          text: 'あああああああああああああああああああああああああ・・・',
+          iconBgColor: Colors.red,
+        ),
+      ],
+    );
+  }
+}
+
+
+class EventCard extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconBgColor;
+
+  EventCard({required this.icon, required this.text, required this.iconBgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: iconBgColor,
+          child: Icon(icon, color: Colors.white),
+        ),
+        title: Text(text),
+      ),
+    );
+  }
+}
+
+
+FlutterAppAuth appAuth = const FlutterAppAuth();
+
+auth() async {
+  try {
+    final AuthorizationTokenResponse? result = await appAuth.authorizeAndExchangeCode(
+      AuthorizationTokenRequest(
+        dotenv.get('CLIENT_ID'),
+        dotenv.get('REDIRECT_URL'),
+        discoveryUrl: dotenv.get('DISCOVERY_URL'),
+        scopes: ['openid'],
+      ),
+    );
+    print(result?.accessToken);
+  } catch (e) {
+    print('Error during authorization: $e');
+    throw e;
   }
 }
