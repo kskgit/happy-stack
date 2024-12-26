5152ca72ee15cb20da399a7d3035f332d142550b
nakao keisuke <r3grashopper193@gmail.com>
2024-12-27 06:59:39 +0900

    graphqlサーバに接続出来るまで

--- lib/graphql_client.dart
+++ lib/graphql_client.dart
@@ -3,7 +3,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 ValueNotifier<GraphQLClient> graphQLClient() {
   final httpLink = HttpLink(
-    'localhost:8000',
+    'http://127.0.0.1:8000/',
   );
 
   final authLink = AuthLink(
