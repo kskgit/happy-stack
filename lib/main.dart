0e2c61d9d659053863116fd839761a83471ee541
nakao keisuke <r3grashopper193@gmail.com>
2025-01-28 23:21:36 +0900

    不要ファイルを削除

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tutorial/edit_screen.dart';
 
 void main() async {
-  // await dotenv.load();
   runApp(
     const ProviderScope(
       child: MyApp(),
