e378764b76f7b5340674966252d2fe346748375f
nakao keisuke <r3grashopper193@gmail.com>
2025-02-01 09:47:12 +0900

    flutter hooksを使わない方法へ変更

--- lib/edit_screen.dart
+++ lib/edit_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tutorial/time_picker.dart';
-import 'package:flutter_tutorial/time_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class EditScreen extends HookConsumerWidget {
+class EditScreen extends ConsumerStatefulWidget {
   const EditScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final selectedDays = useState(List.generate(7, (_) => false));
+  ConsumerState<EditScreen> createState() => _EditScreenState();
+}
 
+class _EditScreenState extends ConsumerState<EditScreen> {
+  String _titel = '';
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -27,19 +32,25 @@ class EditScreen extends HookConsumerWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              Text(_titel),
               const SizedBox(height: 20),
               const Icon(
-                Icons.abc, // ハロウィン風のアイコン
+                Icons.abc,
                 size: 50,
                 color: Colors.orange,
               ),
               const SizedBox(height: 20),
-              const TextField(
-                decoration: InputDecoration(
+              TextField(
+                decoration: const InputDecoration(
                   labelText: 'タイトル',
                   hintText: '呪術廻戦',
                   border: UnderlineInputBorder(),
                 ),
+                onChanged: (value) {
+                  setState(() {
+                    _titel = value; // 入力値を普通の変数に保持
+                  });
+                },
               ),
               const SizedBox(height: 30),
               const Row(
@@ -51,30 +62,30 @@ class EditScreen extends HookConsumerWidget {
                 ],
               ),
               const SizedBox(height: 10),
-              ToggleButtons(
-                isSelected: selectedDays.value,
-                onPressed: (int index) {
-                  selectedDays.value = [
-                    for (var i = 0; i < selectedDays.value.length; i++)
-                      i == index
-                          ? !selectedDays.value[i]
-                          : selectedDays.value[i],
-                  ];
-                },
-                constraints: const BoxConstraints(minWidth: 40, minHeight: 40),
-                color: Colors.grey,
-                selectedColor: Colors.teal,
-                fillColor: Colors.teal.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(10),
-                children: ['月', '火', '水', '木', '金', '土', '日']
-                    .map(
-                      (day) => Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: Text(day, style: const TextStyle(fontSize: 16)),
-                      ),
-                    )
-                    .toList(),
-              ),
+              // ToggleButtons(
+              //   isSelected: selectedDays.value,
+              //   onPressed: (int index) {
+              //     selectedDays.value = [
+              //       for (var i = 0; i < selectedDays.value.length; i++)
+              //         i == index
+              //             ? !selectedDays.value[i]
+              //             : selectedDays.value[i],
+              //     ];
+              //   },
+              //   constraints: const BoxConstraints(minWidth: 40, minHeight: 40),
+              //   color: Colors.grey,
+              //   selectedColor: Colors.teal,
+              //   fillColor: Colors.teal.withOpacity(0.2),
+              //   borderRadius: BorderRadius.circular(10),
+              //   children: ['月', '火', '水', '木', '金', '土', '日']
+              //       .map(
+              //         (day) => Padding(
+              //           padding: const EdgeInsets.symmetric(horizontal: 12),
+              //           child: Text(day, style: const TextStyle(fontSize: 16)),
+              //         ),
+              //       )
+              //       .toList(),
+              // ),
               const SizedBox(height: 30),
               const Row(
                 children: [
@@ -88,7 +99,9 @@ class EditScreen extends HookConsumerWidget {
               const TimePickerWidget(),
               const SizedBox(height: 50),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () async {
+                  await sendDataToApi(_titel);
+                },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.yellow,
                   padding: const EdgeInsets.symmetric(
@@ -125,11 +138,18 @@ class EditScreen extends HookConsumerWidget {
 }
 
 // 時刻選択を表示する関数
-Future<void> _selectTime(BuildContext context, WidgetRef ref) async {
-  final picked = await showTimePicker(
-    context: context,
-    initialTime: TimeOfDay.now(),
-  );
+// Future<void> _selectTime(BuildContext context, WidgetRef ref) async {
+//   final picked = await showTimePicker(
+//     context: context,
+//     initialTime: TimeOfDay.now(),
+//   );
 
-  ref.read(timeStateProvider.notifier).updateTime(picked!);
+//   ref.read(timeStateProvider.notifier).updateTime(picked!);
+// }
+
+// APIにデータを送信する関数を追加
+Future<void> sendDataToApi(String title) async {
+  debugPrint(title);
+  final supabase = Supabase.instance.client;
+  // supabase.from('stocks')
 }
