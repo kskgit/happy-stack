a59de8f113aa2b1a12586130f08ad925dba7bad0
nakao keisuke <r3grashopper193@gmail.com>
2025-03-12 07:28:01 +0900

    ✨ Happinessモデルにシリアライゼーションを追加

--- lib/features/home/domain/happiness.dart
+++ lib/features/home/domain/happiness.dart
@@ -1,25 +1,18 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'happiness.freezed.dart';
+part 'happiness.g.dart';
 
 @freezed
-class Happiness with _$Happiness {
-  Happiness({
-    required this.id,
-    required this.name,
-    required this.dayOfWeek,
-    required this.notificationTime,
-    required this.createdAt,
-  });
+sealed class Happiness with _$Happiness {
+  const factory Happiness({
+    required int id,
+    required String name,
+    required String dayOfWeek,
+    required String notificationTime,
+    required DateTime createdAt,
+  }) = _Happiness;
 
-  @override
-  final int id;
-  @override
-  final String name;
-  @override
-  final String dayOfWeek;
-  @override
-  final String notificationTime;
-  @override
-  final DateTime createdAt;
+  factory Happiness.fromJson(Map<String, dynamic> json) =>
+      _$HappinessFromJson(json);
 }
