093afd911024ed960741ec97419d9a8c5d3a0e48
nakao keisuke <r3grashopper193@gmail.com>
2025-03-28 07:09:14 +0900

    🗑️ 不要なSelectedDayOfWeekクラスを削除し、コードをクリーンアップ

--- lib/features/edit/edit_screen.dart
+++ lib/features/edit/edit_screen.dart
@@ -1,24 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_tutorial/constants/day_of_week.dart';
 import 'package:flutter_tutorial/features/edit/edit_screen_controller.dart';
 import 'package:flutter_tutorial/features/edit/input_form.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-class SelectedDayOfWeek {
-  SelectedDayOfWeek({
-    required this.dayOfWeek,
-  });
-  final DayOfWeek dayOfWeek;
-  bool isSelected = false;
-  Map<String, dynamic> toJson() {
-    return {
-      'name': dayOfWeek.displayValue,
-      'value': isSelected,
-    };
-  }
-}
 
 @RoutePage()
 class EditScreen extends ConsumerWidget {
