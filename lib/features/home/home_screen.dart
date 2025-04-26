c3443e8ebef192747623ebb304d3b4ae9df38b94
nakao keisuke <r3grashopper193@gmail.com>
2025-04-27 07:38:39 +0900

    HomeScreenの曜日カード生成ロジックをリファクタリングし、DayCardウィジェットを新規作成。コードの可読性と再利用性を向上。

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tutorial/constants/day_of_week.dart';
 import 'package:flutter_tutorial/features/home/daily_list.dart';
+import 'package:flutter_tutorial/features/home/widgets/day_card.dart';
 import 'package:flutter_tutorial/features/input_form/registration/registration_screen.dart';
 import 'package:flutter_tutorial/providers/selected_day_provider.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -95,89 +96,21 @@ class HomeScreen extends ConsumerWidget {
   // 曜日カードを生成するメソッド
   List<Widget> _buildDayCards(WidgetRef ref, DayOfWeek selectedDay) {
     final now = DateTime.now();
-    final dayCards = <Widget>[];
+    return List.generate(
+      DayOfWeek.values.length,
+      (i) {
+        final date = DateTime(now.year, now.month, now.day + i);
+        final dayOfWeek = DayOfWeek.values.firstWhere(
+          (day) => day.weekdayIndex == (date.weekday),
+        );
 
-    for (var i = 0; i < DayOfWeek.values.length; i++) {
-      final date = DateTime(now.year, now.month, now.day + i);
-      final dayOfWeek = DayOfWeek.values.firstWhere(
-        (day) => day.weekdayIndex == (date.weekday),
-      );
-      final isSelected = dayOfWeek == selectedDay;
-
-      dayCards.add(
-        Padding(
-          padding: const EdgeInsets.only(right: 12),
-          child: InkWell(
-            onTap: () {
-              // 選択された曜日を更新
-              ref.read(selectedDayProvider.notifier).updateDay(dayOfWeek);
-            },
-            borderRadius: BorderRadius.circular(16),
-            child: Container(
-              width: 80,
-              decoration: BoxDecoration(
-                color: isSelected ? Colors.purple.shade100 : Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    blurRadius: 5,
-                    spreadRadius: 1,
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    dayOfWeek.displayValue,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: isSelected ? Colors.purple : Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    date.day.toString(),
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: isSelected ? Colors.purple : Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  // TODOAPI経由で取得するように変更する
-                  if (i % 2 == 0)
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.circle,
-                          size: 8,
-                          color: isSelected
-                              ? Colors.purple.shade300
-                              : Colors.grey.shade400,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          '${i + 1} Meals',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: isSelected
-                                ? Colors.purple.shade700
-                                : Colors.grey.shade600,
-                          ),
-                        ),
-                      ],
-                    ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    return dayCards;
+        return DayCard(
+          date: date,
+          dayOfWeek: dayOfWeek,
+          isSelected: dayOfWeek == selectedDay,
+          happyCount: i % 2 == 0 ? i + 1 : null, // TODO: API経由で取得するように変更
+        );
+      },
+    );
   }
 }
---
+++ lib/features/home/widgets/day_card.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tutorial/constants/day_of_week.dart';
+import 'package:flutter_tutorial/providers/selected_day_provider.dart';
+
+class DayCard extends ConsumerWidget {
+  const DayCard({
+    required this.date,
+    required this.dayOfWeek,
+    required this.isSelected,
+    required this.happyCount,
+    super.key,
+  });
+
+  final DateTime date;
+  final DayOfWeek dayOfWeek;
+  final bool isSelected;
+  final int? happyCount;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 12),
+      child: InkWell(
+        onTap: () {
+          ref.read(selectedDayProvider.notifier).updateDay(dayOfWeek);
+        },
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          width: 80,
+          decoration: BoxDecoration(
+            color: isSelected ? Colors.purple.shade100 : Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                blurRadius: 5,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                dayOfWeek.displayValue,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: isSelected ? Colors.purple : Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                date.day.toString(),
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: isSelected ? Colors.purple : Colors.black,
+                ),
+              ),
+              const SizedBox(height: 8),
+              if (happyCount != null)
+                _HappyCountBadge(
+                  count: happyCount!,
+                  isSelected: isSelected,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _HappyCountBadge extends StatelessWidget {
+  const _HappyCountBadge({
+    required this.count,
+    required this.isSelected,
+  });
+
+  final int count;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.circle,
+          size: 8,
+          color: isSelected ? Colors.purple.shade300 : Colors.grey.shade400,
+        ),
+        const SizedBox(width: 4),
+        Text(
+          count.toString(),
+          style: TextStyle(
+            fontSize: 10,
+            color: isSelected ? Colors.purple.shade700 : Colors.grey.shade600,
+          ),
+        ),
+      ],
+    );
+  }
+}
