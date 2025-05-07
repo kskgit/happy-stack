7558a3ef02fe136669f44d2b4df13fd227620357
nakao keisuke <r3grashopper193@gmail.com>
2025-05-08 07:23:26 +0900

    WIP:とりあえず絵文字が選択出来るまで

--- lib/features/input_form/common/title_text_field.dart
+++ lib/features/input_form/common/title_text_field.dart
@@ -1,6 +1,7 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 
-class TitleTextField extends StatelessWidget {
+class TitleTextField extends StatefulWidget {
   const TitleTextField({
     required this.onChanged,
     this.initialValue,
@@ -11,33 +12,78 @@ class TitleTextField extends StatelessWidget {
   final String? initialValue;
 
   @override
+  State<TitleTextField> createState() => _TitleTextFieldState();
+}
+
+class _TitleTextFieldState extends State<TitleTextField> {
+  late TextEditingController _textEditingController;
+  bool _isEmojiPickerVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController(text: widget.initialValue);
+    _textEditingController.addListener(() {
+      widget.onChanged(_textEditingController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        const Icon(
-          Icons.lightbulb,
-          size: 50,
-          color: Colors.orange,
-        ),
-        const SizedBox(width: 10),
-        Expanded(
-          child: TextFormField(
-            initialValue: initialValue,
-            decoration: const InputDecoration(
-              labelText: 'タイトル',
-              hintText: 'アニメを見る',
-              border: UnderlineInputBorder(),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _isEmojiPickerVisible = !_isEmojiPickerVisible;
+                });
+              },
+              icon: const Icon(Icons.emoji_emotions),
             ),
-            onChanged: onChanged,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'タイトルを入力してください';
-              }
-              return null;
-            },
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-          ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextFormField(
+                controller: _textEditingController,
+                decoration: const InputDecoration(
+                  labelText: 'タイトル',
+                  hintText: 'アニメを見る',
+                  border: UnderlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'タイトルを入力してください';
+                  }
+                  return null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+              ),
+            ),
+          ],
         ),
+        if (_isEmojiPickerVisible)
+          SizedBox(
+            height: 250,
+            child: EmojiPicker(
+              onEmojiSelected: (category, emoji) {
+                _textEditingController.text += emoji.emoji;
+              },
+              textEditingController: _textEditingController,
+              config: const Config(
+                emojiViewConfig: EmojiViewConfig(
+                  emojiSizeMax: 30,
+                  columns: 7,
+                ),
+              ),
+            ),
+          ),
       ],
     );
   }
